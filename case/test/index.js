eb2003449c171b971f1cca55fa78f78fe0c7b6a0
mytaowu <mytaowu@tencent.com>
2023-06-19 15:15:37 +0800

    feat: 集成mark down

---
+++ case/test/index.js
@@ -0,0 +1,9 @@
+function testMap() {
+    const nodeMap = new Map();
+    nodeMap.set('2023-09-23', { name: 'mytaowu', age: 13 })
+    nodeMap.set('2023-07-31', { name: 'mytaowu', age: 13 })
+    nodeMap.set('2023-05-31', { name: 'mytaowu', age: 13 })
+    nodeMap.set('2023-10-01', { name: 'mytaowu', age: 13 })
+}
+
+testMap();
